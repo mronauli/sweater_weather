2c1f16b6a452c612a20583409cb2cc16daea214a
Maria Ronauli <mronauli@gmail.com>
2020-04-19 11:21:27 -0600

    change sweater weather app to api only app

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ Bundler.require(*Rails.groups)
 
 module WhetherSweater
   class Application < Rails::Application
+
+    config.api_only = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
