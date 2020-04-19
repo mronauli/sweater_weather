572001acb18b935a3534d232a96f0d883dd6fda2
Maria Ronauli <mronauli@gmail.com>
2020-04-19 14:02:47 -0600

    add geocode object to filter data

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -6,15 +6,13 @@ class Api::V1::ForecastsController < ApplicationController
     end
     response = conn.get("/maps/api/geocode/json")
     geocode_data = JSON.parse(response.body, symbolize_names: true)
-
-    lat = geocode_data[:results][0][:geometry][:bounds][:northeast][:lat]
-    long = geocode_data[:results][0][:geometry][:bounds][:northeast][:lng]
+    filtered_data = Geocode.new(geocode_data)
 
     conn = Faraday.new("https://api.openweathermap.org") do |req|
       req.params["appid"] = ENV["WEATHER_API"]
       req.params["units"] = "imperial"
-      req.params["lat"] = lat
-      req.params["lon"] = long
+      req.params["lat"] = filtered_data.lat
+      req.params["lon"] = filtered_data.long
       req.params["timezone"] = "UTC -7"
     end
     response = conn.get("/data/2.5/onecall")
