f668ff167ef343690fb5b09a4a793c43501d7f23
Maria Ronauli <mronauli@gmail.com>
2020-04-19 14:23:32 -0600

    add current weather to controller

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -3,5 +3,6 @@ class Api::V1::ForecastsController < ApplicationController
     geocode_data = GeocodeService.new.get_data(params["location"])
     geocode = Geocode.new(geocode_data)
     weather_data = WeatherService.new.get_data(geocode.lat, geocode.long)
+    current_weather = CurrentWeather.new(weather_data)
   end
 end
