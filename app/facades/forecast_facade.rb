cb241ef04ff2ddd733d36621abd18bbde8536407
Maria Ronauli <mronauli@gmail.com>
2020-04-19 17:56:53 -0600

    add id to forecast facade

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,11 +1,12 @@
 class ForecastFacade
-  attr_reader :geocode, :location, :current_weather, :hourly_weather, :daily_weather
+  attr_reader :geocode, :location, :current_weather, :hourly_weather, :daily_weather, :id
   def initialize(location)
     @location = location
     @geocode = Geocode.new(geocode_data)
     @current_weather = CurrentWeather.new(weather_data)
     @hourly_weather = weather_data[:hourly][0,8].map { |hour| HourlyWeather.new(hour) }
     @daily_weather = weather_data[:daily][0,7].map { |day| DailyWeather.new(day) }
+    @id = nil
   end
 
   def geocode_data
