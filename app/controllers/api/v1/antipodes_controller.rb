3ed26252e181258e46c5455660d15316180277d2
Maria Ronauli <mronauli@gmail.com>
2020-04-20 10:55:00 -0600

    refactor with antipode service

--- app/controllers/api/v1/antipodes_controller.rb
+++ app/controllers/api/v1/antipodes_controller.rb
@@ -2,14 +2,9 @@ class Api::V1::AntipodesController < ApplicationController
   def show
     data = GeocodeService.new.get_data_via_location(params["location"])
     geocode = Geocode.new(data)
-    conn = Faraday.new("http://amypode.herokuapp.com") do |req|
-      req.headers["api_key"] = ENV["AMY_API"]
-      req.params["lat"] = geodode.lat
-      req.params["long"] = geocode.long
-    end
-    response = conn.get("/api/v1/antipodes")
-    antipode_data = JSON.parse(response.body, symbolize_names: true)
+    antipode_data = AntipodeService.new.get_data(geocode.lat, geocode.long)
     antipode = Antipode.new(antipode_data)
+    require "pry"; binding.pry
     anti_pode_location_data = GeocodeService.new.get_data_via_latlong(antipode.lat, antipode.long)
     city_name = anti_pode_location_data[:results][3][:formatted_address]
     data = WeatherService.new.get_data_via_city(city_name)
