4aad4ef32dd6daa9d23599915a1c753447f30dcd
Maria Ronauli <mronauli@gmail.com>
2020-04-20 10:01:25 -0600

    add service to get location using lat long

--- app/controllers/api/v1/antipodes_controller.rb
+++ app/controllers/api/v1/antipodes_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::AntipodesController < ApplicationController
   def show
-    data = GeocodeService.new.get_data(params["location"])
+    data = GeocodeService.new.get_data_via_location(params["location"])
     lat = data[:results][0][:geometry][:location][:lat]
     long = data[:results][0][:geometry][:location][:lng]
     conn = Faraday.new("http://amypode.herokuapp.com") do |req|
@@ -10,6 +10,8 @@ class Api::V1::AntipodesController < ApplicationController
     end
     response = conn.get("/api/v1/antipodes")
     antipode_data = JSON.parse(response.body, symbolize_names: true)
-    require "pry"; binding.pry
+    latt = antipode_data[:data][:attributes][:lat]
+    longg = antipode_data[:data][:attributes][:long]
+    anti_pode_location_data = GeocodeService.new.get_data_via_latlong(latt, longg)
   end
 end
