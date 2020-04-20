7bf2306dea90d8bfcdf147c6fb50222bbc385c6a
Maria Ronauli <mronauli@gmail.com>
2020-04-20 11:53:56 -0600

    convert antipode weather to json

--- app/controllers/api/v1/antipodes_controller.rb
+++ app/controllers/api/v1/antipodes_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::AntipodesController < ApplicationController
   def show
     antipode = AntipodeFacade.new(params["location"])
+    render json: AntipodeSerializer.new(antipode)
   end
 end
