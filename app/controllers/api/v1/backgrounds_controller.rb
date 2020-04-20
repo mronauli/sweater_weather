8d64fc4af82ce23e79819758c16a81c06a21bcd8
Maria Ronauli <mronauli@gmail.com>
2020-04-19 18:34:52 -0600

    add background facade to controller

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::BackgroundsController < ApplicationController
   def show
-    data = BackgroundService.new.get_data(params["location"])
-    image_url = Background.new(data)
+    background = BackgroundFacade.new(params[:location])
   end
 end
