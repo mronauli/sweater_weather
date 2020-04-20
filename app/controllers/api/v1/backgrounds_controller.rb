343baf6a6168c322a30736bf6c6503609efe69ba
Maria Ronauli <mronauli@gmail.com>
2020-04-19 18:32:25 -0600

    add background service to controller

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,12 +1,6 @@
 class Api::V1::BackgroundsController < ApplicationController
   def show
-    conn = Faraday.new("https://api.pexels.com") do |req|
-      req.headers["Authorization"] = ENV["PEXEL_API"]
-      req.params["query"] = params["location"][0..-3]
-      req.params["per_page"] = 1
-    end
-    response = conn.get("/v1/search")
-    background_data = JSON.parse(response.body, symbolize_names: true)
-    image_url = Background.new(background_data)
+    data = BackgroundService.new.get_data(params["location"])
+    image_url = Background.new(data)
   end
 end
