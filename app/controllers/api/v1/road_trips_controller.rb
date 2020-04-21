53effd4f0f3ddce41d746335dff723feca0b79e9
Maria Ronauli <mronauli@gmail.com>
2020-04-21 03:27:45 -0600

    convert road trip data into json

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::RoadTripsController < ApplicationController
     user =  User.find_by_api_key(params[:api_key])
     if user
       roadtrip = RoadTripFacade.new(params[:origin], params[:destination])
+      render json: RoadTripSerializer.new(roadtrip)
     else
       render json: {status: 401, text: 'Unauthorized.'}
     end
