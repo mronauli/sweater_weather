772aa434b99400b18662672498a00454cddf7d9d
Maria Ronauli <mronauli@gmail.com>
2020-04-21 00:16:31 -0600

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       get "/antipode", to: "antipodes#show"
       post "/users", to: "users#create"
       post "/sessions", to: "sessions#create"
+      post "/road_trip", to: "road_trips#create"
     end
   end
 end
