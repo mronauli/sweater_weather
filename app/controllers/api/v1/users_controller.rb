6a4f6ba0111bcb3245278171a933bfaaa11084be
Maria Ronauli <mronauli@gmail.com>
2020-04-21 05:42:15 -0600

    add sad path

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    new_user = User.create(user_params)
-    render json: UsersSerializer.new(new_user), status: 201
+    user = User.find_by_email(params[:email])
+    if user.nil?
+      new_user = User.create(user_params)
+      render json: UsersSerializer.new(new_user), status: 201
+    else
+      render json: {status: 400, text: 'Invalid credentials.'}
+    end
   end
 
   private
