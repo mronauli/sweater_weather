adac7474cf9ac6a1d129f9b4b1e5150cbf6f0238
Maria Ronauli <mronauli@gmail.com>
2020-04-22 10:08:02 -0600

    add yelp service to controller

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -2,16 +2,9 @@ class Api::V1::MunchiesController < ApplicationController
   def show
      direction_data = GeocodeService.new.get_directions(params[:start], params[:end])
      direction = Direction.new(direction_data)
-     conn = Faraday.new("https://api.yelp.com") do |req|
-       req.headers["Authorization"] = ENV["YELP_API"]
-       req.params["term"] = "food"
-       req.params["categories"] = params[:food]
-       req.params["location"] = params[:end]
-       req.params["open_at"] = (Time.now + (direction.travel_time.tr('^0-9', '').to_i / 60)).to_i
-     end
-     response = conn.get("/v3/businesses/search")
-     restaurant_data = JSON.parse(response.body, symbolize_names: true)
+     restaurant_data = YelpService.new.get_data(params[:food], params[:end], (Time.now + (direction.travel_time.tr('^0-9', '').to_i / 60).hours).to_i)
      restaurant_name = restaurant_data[:businesses][0][:name]
      restaurant_address = restaurant_data[:businesses][0][:location][:display_address]
+     require "pry"; binding.pry
   end
 end
