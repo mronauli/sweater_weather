788f4f18b39c87a4f9480e419c37f8e00d058dbb
Maria Ronauli <mronauli@gmail.com>
2020-04-22 10:11:57 -0600

    add roadtrip facade to controller

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -5,6 +5,9 @@ class Api::V1::MunchiesController < ApplicationController
      restaurant_data = YelpService.new.get_data(params[:food], params[:end], (Time.now + (direction.travel_time.tr('^0-9', '').to_i / 60).hours).to_i)
      restaurant_name = restaurant_data[:businesses][0][:name]
      restaurant_address = restaurant_data[:businesses][0][:location][:display_address]
+     munchie_weather = RoadTripFacade.new(params[:start], params[:end], params["food"])
+     hourly_weather = munchie_weather.forecast
+     forecast = hourly_weather.description
      require "pry"; binding.pry
   end
 end
