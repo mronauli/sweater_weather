f3a56f849a103a6259eea01416e04a0b219872d4
Maria Ronauli <mronauli@gmail.com>
2020-04-22 10:27:50 -0600

    clean up munchies controller

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,11 +1,5 @@
 class Api::V1::MunchiesController < ApplicationController
   def show
-     direction_data = GeocodeService.new.get_directions(params[:start], params[:end])
-     direction = Direction.new(direction_data)
-     restaurant_data = YelpService.new.get_data(params[:food], params[:end], (Time.now + (direction.travel_time.tr('^0-9', '').to_i / 60).hours).to_i)
-     restaurant_name_address = Restaurant.new(restaurant_data)
-     munchie_weather = RoadTripFacade.new(params[:start], params[:end], params["food"])
-     hourly_weather = munchie_weather.forecast
-     forecast = hourly_weather.description
+     munchie_trip = RoadTripFacade.new(params[:start], params[:end], params["food"])
   end
 end
