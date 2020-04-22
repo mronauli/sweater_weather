deff720e41ca936b0a91bfea70ecd01d139c5c7b
Maria Ronauli <mronauli@gmail.com>
2020-04-22 11:13:52 -0600

    convert address into string

--- app/poros/restaurant.rb
+++ app/poros/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant
   def initialize(data)
     @name = data[:businesses][0][:name]
-    @address = data[:businesses][0][:location][:display_address]
+    @address = data[:businesses][0][:location][:display_address].join(", ")
   end
 end
