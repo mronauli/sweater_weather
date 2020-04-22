5adc471e4f2ca18e529246ac141df917b1ea2762
Maria Ronauli <mronauli@gmail.com>
2020-04-22 10:27:10 -0600

    edit forecast calc, add yelp service and restaurant poro to facade

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,16 +1,17 @@
 class RoadTripFacade
-  attr_reader :origin, :destination, :direction, :forecast, :id, :food
+  attr_reader :origin, :destination, :direction, :forecast, :id, :food, :restaurant
   def initialize(origin, destination, food = nil)
     @origin = origin
     @destination = destination
     @direction = Direction.new(direction_data)
     if travel_minutes > 60
-      @forecast = HourlyWeather.new(weather_data[:hourly][travel_time.tr('^0-9', '').to_i - 1])
+      @forecast = HourlyWeather.new(weather_data[:hourly][travel_time.tr('^0-9', '').to_i])
     else
       @forecast = HourlyWeather.new(weather_data[:hourly][0])
     end
     @id = nil
     @food = food
+    @restaurant = Restaurant.new(restaurant_data)
   end
 
   def arrival_forecast
@@ -31,5 +32,9 @@ class RoadTripFacade
 
   def weather_data
     WeatherService.new.get_data_via_latlong(direction.lat, direction.long)
+  end
+
+  def restaurant_data
+    YelpService.new.get_data(food, destination, (Time.now + (direction.travel_time.tr('^0-9', '').to_i / 60).hours).to_i)
   end
 end
