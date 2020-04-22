c7f288bef87cfb55fc22cf75ca7759d6faaf943c
Maria Ronauli <mronauli@gmail.com>
2020-04-22 09:57:06 -0600

    change params to get back chinese food instead of mexican

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -5,12 +5,12 @@ class Api::V1::MunchiesController < ApplicationController
      conn = Faraday.new("https://api.yelp.com") do |req|
        req.headers["Authorization"] = ENV["YELP_API"]
        req.params["term"] = "food"
-       req.params["categories"] = params["food"]
-       req.params["latitude"] = direction.lat
-       req.params["longitude"] = direction.long
+       req.params["categories"] = params[:food]
+       req.params["location"] = params[:end]
        req.params["open_at"] = (Time.now + (direction.travel_time.tr('^0-9', '').to_i / 60)).to_i
      end
      response = conn.get("/v3/businesses/search")
      restaurant_data = JSON.parse(response.body, symbolize_names: true)
+     require "pry"; binding.pry
   end
 end
