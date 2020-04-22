40cd2f26b004a795a56c4ba96d9a2957266238d0
Maria Ronauli <mronauli@gmail.com>
2020-04-22 10:23:13 -0600

    add restaurant to controller

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -3,11 +3,9 @@ class Api::V1::MunchiesController < ApplicationController
      direction_data = GeocodeService.new.get_directions(params[:start], params[:end])
      direction = Direction.new(direction_data)
      restaurant_data = YelpService.new.get_data(params[:food], params[:end], (Time.now + (direction.travel_time.tr('^0-9', '').to_i / 60).hours).to_i)
-     restaurant_name = restaurant_data[:businesses][0][:name]
-     restaurant_address = restaurant_data[:businesses][0][:location][:display_address]
+     restaurant_name_address = Restaurant.new(restaurant_data)
      munchie_weather = RoadTripFacade.new(params[:start], params[:end], params["food"])
      hourly_weather = munchie_weather.forecast
      forecast = hourly_weather.description
-     require "pry"; binding.pry
   end
 end
