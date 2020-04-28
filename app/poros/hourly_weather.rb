c7c32f139d20107358bfe966e64e32b278769ab0
Maria Ronauli <mronauli@gmail.com>
2020-04-28 00:50:39 -0600

    refactor road trip facade

--- app/poros/hourly_weather.rb
+++ app/poros/hourly_weather.rb
@@ -1,7 +1,9 @@
 class HourlyWeather
+  attr_reader :temp, :description
   def initialize(data)
     @date = Time.at(data[:dt]).strftime("%-I %p")
     @temp = data[:temp].round
     @icon = data[:weather][0][:icon]
+    @description = data[:weather][0][:description]
   end
 end
